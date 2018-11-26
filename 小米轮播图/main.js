21fe90efe9f09296692cd1769d5515a09f24ff7e
lvbin <472245767@qq.com>
2018-11-26 14:25:20 +0800

    小米轮播图

---
+++ 小米轮播图/main.js
@@ -0,0 +1,32 @@
+let allButtons = $('#menu button');
+let n = 0;
+for (let i = 0; i < allButtons.length; i++) {
+    $(allButtons[i]).on('click', function (e) {
+        let index = $(e.currentTarget).index();
+        let pixel = index * (-920);
+        $('#images').css({transform: 'translateX(' + pixel + 'px)'});
+        n = index;
+        $(allButtons).eq(n).addClass('active').siblings().removeClass('active')
+    });
+}
+
+let buttonLength = allButtons.length;
+
+let timer = setTime();
+
+$('#main').on('mouseenter', function () {
+    window.clearInterval(timer);
+});
+$('#main').on('mouseleave', function () {
+    timer = setTime();
+});
+
+function setTime() {
+    return setInterval(function () {
+        n++;
+        if (n > buttonLength - 1) {
+            n = 0;
+        }
+        $(allButtons).eq(n).trigger('click');
+    }, 3000);
+}
